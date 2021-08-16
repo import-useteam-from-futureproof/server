42832e3237a7177368a6a7901949c3c238aefffe
James Derrick <jediblob@aol.com>
2021-08-16 18:10:55 +0100

    change dbConfig to use Atlas connection

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -1,12 +1,27 @@
 const { MongoClient } = require('mongodb');
-const connectionUrl = process.env.DB_CONNECTION;
-
-const dbName = process.env.DB_NAME;
+require('dotenv').config();
 
 const init = async () => {
-	let client = await MongoClient.connect(connectionUrl);
-	console.log('connected to database!', dbName);
-	return client.db(dbName);
+	const uri =
+		'mongodb+srv://' +
+		process.env.ATLAS_USER +
+		':' +
+		process.env.ATLAS_PASS +
+		'@pursuit-of-trivia.udq8q.mongodb.net/' +
+		process.env.ATLAS_DB +
+		'?retryWrites=true&w=majority';
+
+	console.log(uri);
+
+	const mongoClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+	try {
+		// Connect to the MongoDB cluster
+		let client = await mongoClient.connect();
+		return client.db(process.env.ATLAS_DB);
+	} catch (e) {
+		console.error(e);
+	}
 };
 
 module.exports = { init };

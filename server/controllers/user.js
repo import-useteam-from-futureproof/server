b741d9929224c07e7b74b8a64443d339cb0578e1
James Derrick <jediblob@aol.com>
2021-08-16 13:55:06 +0100

    add single user GET route

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,6 +3,15 @@ const router = express.Router();
 
 const User = require('../models/User');
 
+router.get('/:id', async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id);
+		res.json(user);
+	} catch (err) {
+		res.status(404).json({ err });
+	}
+});
+
 router.post('/', async (req, res) => {
 	try {
 		const user = await User.create(req.body.id, req.body.username, req.body.avatar_url);

a169d3225d50b359b0beb158072fb7de9aab340e
Velo <hartley.max@outlook.com>
2021-08-16 10:48:02 +0100

    stub and mount profile page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { Profile } from './pages';
 
 export default function App() {
 	return (
 		<>
 			<Switch>
+				<Route path="/profile">
+					<Profile />
+				</Route>
 				<Route path="/">
 					<h1>Hello world!</h1>
 				</Route>
